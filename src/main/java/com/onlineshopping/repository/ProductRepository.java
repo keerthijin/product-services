5952acda767acc9eed4d7aedd1338c7cd746fbc9
keerthi balan <keerthi.balan@ust.com>
2021-10-07 21:46:09 +0530

    initialcommit

---
+++ src/main/java/com/onlineshopping/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.onlineshopping.repository;
+
+import com.onlineshopping.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
+
